de975746090efc2f724cf4eab3c2e81c5bf8ee51
uint82 <abrorhilmi6@gmail.com>
2026-01-16 10:46:56 +0700

    feat: added logic to render pdf

--- src/ts/pages/editor.ts
+++ src/ts/pages/editor.ts
@@ -1,9 +1,10 @@
 import { router } from "../utils/router";
 import { appStore } from "../utils/store";
+import { renderPdfToCanvas } from "../utils/pdf-render";
 import type { Page } from "../utils/router";
 
 export const EditorPage: Page = {
-  mount() {
+  async mount() {
     console.log("Mounting Editor...");
 
     const file = appStore.getFile();
@@ -16,7 +17,11 @@ export const EditorPage: Page = {
     const view = document.getElementById("editor-view");
     view?.classList.remove("hidden");
 
-    console.log("Ready to edit:", file.name);
+    const canvas = document.getElementById("the-canvas") as HTMLCanvasElement;
+
+    if (canvas) {
+      await renderPdfToCanvas(file, canvas);
+    }
 
     const btn = document.getElementById("back-btn");
     if (btn) {
@@ -29,6 +34,12 @@ export const EditorPage: Page = {
     const view = document.getElementById("editor-view");
     view?.classList.add("hidden");
 
-    // TODO: Cleanup canvas memory disini
+    const canvas = document.getElementById("the-canvas") as HTMLCanvasElement;
+    if (canvas) {
+      const ctx = canvas.getContext("2d");
+      ctx?.clearRect(0, 0, canvas.width, canvas.height);
+      canvas.width = 0;
+      canvas.height = 0;
+    }
   },
 };
